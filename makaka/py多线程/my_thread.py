2ae570c59a567be6d83e66ee000c1025fcfaa532
makaka <75128685+makk521@users.noreply.github.com>
2023-03-09 16:28:29 +0800

    update

--- makaka/py多线程/my_thread.py
+++ makaka/py多线程/my_thread.py
@@ -16,3 +16,21 @@ def func2():
 if __name__ == '__main__':
     Thread(target = func1).start()
     Thread(target = func2).start()
+
+# 带参数的
+
+# from threading import Thread
+# import time
+# def func1(a):
+#     while(1):
+#         print(a)
+#         time.sleep(1)   
+
+# def func2(a):
+#     while(1):
+#         print(a)
+#         time.sleep(1)
+
+# if __name__ == '__main__':
+#     Thread(target = func1,args=("a")).start()
+#     Thread(target = func2,args=("b")).start()
