0effa3af9dbdb07786ddbb662a475ba797a4f313
makaka <75128685+makk521@users.noreply.github.com>
2023-03-10 17:53:51 +0800

    socket

--- makaka/socket/cloud_receive.py
+++ makaka/socket/cloud_receive.py
@@ -1,38 +1,45 @@
+'''
+Accepts data from both ports simultaneously
+'''
 import socket
 import sys
+from threading import Thread
 
+def socket_service_data(address):
+    """
+    Connect and receive data
 
-def socket_service_data():
-    # 连接
+    Arguments:
+    address   -   ('cloud private mac',port)
+    """
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(ADDR)  # 在不同主机或者同一主机的不同系统下使用实际ip
+        s.bind(address)  # 在不同主机或者同一主机的不同系统下使用实际ip
         s.listen(10)
     except socket.error as msg:
         print(msg)
         sys.exit(1)
-    print("Wait for Connection..................")
+    print("..................Wait for Connection..................")
 
     sock, addr = s.accept()
     
     while True:
-        # 收
         buf = sock.recv(BUFSIZ)  # 接收数据
         buf1 = buf.decode('utf-8')  # 解码
         if not buf:
             break
         print('Received message:', buf1)
-    # 关闭socket
     sock.close()
+
 
 
 if __name__ == '__main__':
     # 初始化
-    name = socket.gethostname()
-    HOST = socket.gethostbyname('10.0.12.13')  # 获取阿里云服务器私网IP，使用ifconfig可查询
-    PORT = 7789
+    HOST = '10.0.12.13'
     BUFSIZ = 1024
-    ADDR = (HOST, PORT)
+    ADDR1 = (HOST, 7789)
+    ADDR2 = (HOST, 5000)
 
-    socket_service_data()
+    Thread(target = socket_service_data,args=(ADDR1,)).start()
+    Thread(target = socket_service_data,args=(ADDR2,)).start()
--- makaka/socket/rasp_post.py
+++ makaka/socket/rasp_post.py
@@ -1,37 +1,36 @@
+'''
+Simultaneously transmit two pieces of data to the cloud through two ports
+'''
 import socket
 import sys
+import time
+from threading import Thread
 
-send_data = str((1,'2022.12',1))
-def sock_client_data():
-    # 连接
+
+def sock_client_data(address,send_data):
+    """
+    Connect to cloud and send data(every three seconds)
+
+    Arguments:
+    address    - ('cloud public mac', port)
+    send_data  
+    """
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(ADDR)
+        s.connect(address)
     except socket.error as msg:
         print(msg)
         print(sys.exit(1))
 
     while True:
-        # 发
-        data = input("input data:")  # 输入要传输的数据
-        if not data:
-            break
         s.send(send_data.encode('utf-8'))  # 将要传输的数据编码发送，如果是字符数据就必须要编码发送
-        print(type(send_data.encode('utf-8')))
-        # # 收
-        # data = s.recv(BUFSIZ)
-        # if not data:
-        #     break
-        # print(data.decode('utf-8'))
-    # 关闭socket
-    s.close()
+        time.sleep(3)
 
 
 if __name__ == '__main__':
-    # 初始化
-    HOST = '124.223.76.58'  # 比如 99.100.101.102是你的服务器的公网IP
-    PORT = 7789  # IP开放的socket端口
-    BUFSIZ = 1024
-    ADDR = (HOST, PORT)
+    ADDR1 = ('124.223.76.58', 7789)
+    ADDR2 = ('124.223.76.58', 5000)
 
-    sock_client_data()
+    Thread(target = sock_client_data,args=(ADDR1,"a")).start()
+    Thread(target = sock_client_data,args=(ADDR2,"b")).start()
+
