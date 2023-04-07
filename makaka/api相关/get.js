df691d85261f46de1bb2475220bb2d4ab01d28b3
makaka <75128685+makk521@users.noreply.github.com>
2023-04-07 18:02:58 +0800

    update

--- makaka/api相关/get.js
+++ makaka/api相关/get.js
@@ -1,14 +1,18 @@
-// const fun = async function() {
-//     let res = await  fetch('http://43.153.21.199:5000/',{
-//         method: "POST",
-
-//         body: '你好'
-//     });
-//     let data = await res.json();
-//     console.log(data)
+const fun = async function() {
+    let res = await  fetch('http://43.153.21.199:5000/',{
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({data: 'hello world'})
+    });
+    let data = await res.json();
+    console.log(data)
     
-// }
-// fun()
+}
+fun()
+
+
 
 // fetch('http://43.153.21.199:5000/', {
 //     method: 'POST',
@@ -36,20 +40,20 @@
 // }
 // fun()
 
-fetch('http://43.153.21.199:5000/', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({data: 'hello world'})
-})
-.then(response => {
-    if (response.ok) {
-        console.log('Request succeeded!');
-    } else {
-        console.log('Request failed.');
-    }
-})
-.catch(error => {
-    console.log('Request failed:', error);
-});
+// fetch('http://43.153.21.199:5000/', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({data: 'hello world'})
+// })
+// .then(response => {
+//     if (response.ok) {
+//         console.log('Request succeeded!');
+//     } else {
+//         console.log('Request failed.');
+//     }
+// })
+// .catch(error => {
+//     console.log('Request failed:', error);
+// });
