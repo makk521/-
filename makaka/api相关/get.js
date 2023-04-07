3de7762c9c8d94e0fb602ba02a452c74ac2a6660
makaka <75128685+makk521@users.noreply.github.com>
2023-04-07 18:00:09 +0800

    update

--- makaka/api相关/get.js
+++ makaka/api相关/get.js
@@ -1,8 +1,45 @@
+// const fun = async function() {
+//     let res = await  fetch('http://43.153.21.199:5000/',{
+//         method: "POST",
 
-fetch('/api/endpoint', {
+//         body: '你好'
+//     });
+//     let data = await res.json();
+//     console.log(data)
+    
+// }
+// fun()
+
+// fetch('http://43.153.21.199:5000/', {
+//     method: 'POST',
+
+//     body: JSON.stringify({
+//       key1: 111,
+//       key2: '111',
+//       key3: 111
+//     })
+//   })
+
+// const fun = async function() {
+//     let res = await  fetch('http://43.153.21.199:5000/',{
+//         method: "POST",
+//         headers: {
+//             "X-APISpace-Token":"23rtlq8nj6ctlbd7gdnb6mko5lshcjgr",
+//             "Authorization-Type":"apikey",
+//             "Content-Type":"application/json"
+//         },
+//         body: '{"cpCode":"YUNDA",     "mailNo":"4329504801266xxxx",     "tel":"0000",      "orderType":"asc"}'
+//     });
+//     let data = await res.json();
+//     console.log(data)
+    
+// }
+// fun()
+
+fetch('http://43.153.21.199:5000/', {
     method: 'POST',
     headers: {
-        'Content-Type': 'http://43.153.21.199:5000/'
+        'Content-Type': 'application/json'
     },
     body: JSON.stringify({data: 'hello world'})
 })
