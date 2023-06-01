180f65ff57794cff6cac32b936332d80b0796d1b
makaka <75128685+makk521@users.noreply.github.com>
2023-06-01 17:43:39 +0800

    update

--- makaka/swiss_copy/th.py
+++ makaka/swiss_copy/th.py
@@ -1,8 +1,19 @@
+import threading
+import time
 class MyThread(threading.Thread):
-    def __init__(self, params):
-        pass
+    def __init__(self, name, n):
+        #super(MyThread, self).__init__()
+        self.name = name
+        self.n = n
     def run(self):
-       pass
-t1 = MyThread(params)
-t.setDaemon(True)
-t1.start()
+        while True:
+            print("Thread ", self.name, "is running")
+            time.sleep(self.n)
+def main():
+    t1 = MyThread("t1", 3)
+    t2 = MyThread("t2", 2)
+    t1.start()
+    t2.start()
+if __name__ == "__main__":
+    print("嗨客网(www.haicoder.net)")
+    main()
